36b4f3a8d2683cb01da0b2c56ddc196b4f7d9cc3
Gan Tu <github@tugan.dev>
2021-12-09 22:14:11 -0500

    finish day 9 puzzle 2

--- day9/day9.py
+++ day9/day9.py
@@ -22,10 +22,39 @@ def puzzle1(data):
   return total
 
 def puzzle2(data):
-  # TODO
-  return
+  basinSizes = []
+  visited = [[False for _ in range(len(data[0]))] for _ in range(len(data))]
+  for r in range(len(data)):
+    for c in range(len(data[r])):
+      if data[r][c] == 9:
+        visited[r][c] = True
+        continue
+      if visited[r][c]:
+        continue
+      # bread first search fan out
+      queue = [(r,c)]
+      size = 1
+      visited[r][c] = True
+      while len(queue) > 0:
+        y,x = queue.pop(0)
+        # print(basinSizes, size, (y,x))
+        for dy, dx in [[0,1],[0,-1],[1,0],[-1,0]]:
+          y2, x2 = y + dy, x + dx
+          if y2 >= 0 and y2 < len(data):
+            if x2 >= 0 and x2 < len(data[r]):
+              if data[y2][x2] == 9:
+                visited[y2][x2] = True
+                continue
+              if visited[y2][x2]:
+                continue
+              size += 1
+              queue.append((y2, x2))
+              visited[y2][x2] = True
+      basinSizes.append(size)
+  basinSizes = sorted(basinSizes, reverse=True)
+  return basinSizes[0] * basinSizes[1] * basinSizes[2]
 
 print("example1: ", puzzle1(parse_input("day9.example")))
 print("puzzle1: ", puzzle1(parse_input("day9.input")))
-# print("example2: ", puzzle2(parse_input("day9.example")))
-# print("puzzle2: ", puzzle2(parse_input("day9.input")))
+print("example2: ", puzzle2(parse_input("day9.example")))
+print("puzzle2: ", puzzle2(parse_input("day9.input")))
